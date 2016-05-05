0c4b94ee4227b5ec4a168812226efbc85ccda45b
CyrilProject <cyril.rioche@wanadoo.fr>
2016-05-05 10:02:42 +0200

    Correction servomoteur
    -Correction de la fonction de conversion pour donner le bon timer selon
    l'angle

--- servomotor.c
+++ servomotor.c
@@ -6,6 +6,6 @@ u16 servoPulseWidth;
 void TurnWheels(s16 degree)
 {
     T1CONbits.TON = 0; //Stop Timer1 
-    servoPulseWidth=22.26*degree+3006;
+    servoPulseWidth=20*degree+3006;
     T1CONbits.TON = 1; //Start Timer1 
 }
